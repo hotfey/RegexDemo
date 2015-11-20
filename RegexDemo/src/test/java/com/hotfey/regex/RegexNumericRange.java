606e47882242b5843f870a5db7c429966ebaef2d
hotfey <hotfey@foxmail.com>
2015-11-21 03:19:28 +0800

    RegexNumericRange

--- RegexDemo/src/test/java/com/hotfey/regex/RegexNumericRange.java
+++ RegexDemo/src/test/java/com/hotfey/regex/RegexNumericRange.java
@@ -6,41 +6,43 @@ import java.util.regex.Pattern;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class RegexTimestampRange {
+public class RegexNumericRange {
 	@Ignore
 	@Test
-	public void a() {
+	public void range() {
 		String startRow = "1448017934380";
-		String stopRow = "1448917934380";
+		String stopRow = "1448017934380";
+		startRow = "100000001";
+		stopRow = "100000001";
 
-		String regex = regexString(startRow, stopRow);
+		int length = currentTimeMillis().length();
+		length = 9;
+		String regex = regexString(startRow, stopRow, length);
 		if (regex.isEmpty()) {
 			System.out.println("regex empty!");
 		} else {
 			System.out.println(regex);
 			String input = "";
-			for (int i = 0; i < 2; i++) {
-				switch (i) {
-				case 0:
-					input = startRow;
-					break;
-				case 1:
-					input = stopRow;
-					break;
-				default:
-					break;
-				}
+			int ok = 0;
+			int no = 0;
+			for (Long i = Long.parseLong(startRow); i <= Long.parseLong(stopRow); i++) {
+				input = i.toString();
 				Pattern pattern = Pattern.compile(regex);
 				Matcher matcher = pattern.matcher(input);
 				if (matcher.find()) {
-					System.out.println(matcher.group());
+					ok++;
+				} else {
+					no++;
 				}
 			}
+			System.out.println("all" + (Long.parseLong(startRow) - Long.parseLong(stopRow) - 1));
+			System.out.println("ok:" + ok);
+			System.out.println("no:" + no);
 		}
 	}
 
-	private String regexString(String startRow, String stopRow) {
-		int length = currentTimeMillis().length();
+	private String regexString(String startRow, String stopRow, int length) {
+
 		if (startRow.length() != length) {
 			return "";
 		}
@@ -65,29 +67,32 @@ public class RegexTimestampRange {
 			}
 		}
 
-		return createRegex(baseString, startRowVariable, stopRowVariable, i);
+		if (i == 0) {
+			return baseString;
+		} else {
+			return createRegex(baseString, startRowVariable, stopRowVariable, i);
+		}
 	}
 
 	private String createRegex(String baseString, String startRowVariable, String stopRowVariable, int length) {
-		if (length == 0) {
-			return "";
-		}
 		char[] startChar = startRowVariable.toCharArray();
 		char[] stopChar = stopRowVariable.toCharArray();
 		StringBuffer stringBuffer = new StringBuffer();
 		stringBuffer.append(baseString);
 
-		stringBuffer.append("((");
-
+		stringBuffer.append("[");
 		stringBuffer.append(startChar[0]);
+		stringBuffer.append("-");
+		stringBuffer.append(stopChar[0]);
+		stringBuffer.append("]");
 
+		stringBuffer.append("((");
 		for (int i = 0; i < length - 1; i++) {
-			stringBuffer.append("\\d");
 			stringBuffer.append("(?<!");
 			for (int j = 0; j <= i; j++) {
 				stringBuffer.append(startChar[j]);
 			}
-			int temp = Integer.parseInt(startChar[i + 1] + "");
+			int temp = Integer.parseInt(String.valueOf(startChar[i + 1]));
 			if (temp == 0) {
 				stringBuffer.append("[^0-");
 				stringBuffer.append("9])");
@@ -97,18 +102,13 @@ public class RegexTimestampRange {
 				stringBuffer.append("])");
 			}
 		}
-
 		stringBuffer.append(")|(");
-
-		stringBuffer.append(stopChar[0]);
-
 		for (int i = 0; i < length - 1; i++) {
-			stringBuffer.append("\\d");
 			stringBuffer.append("(?<!");
 			for (int j = 0; j <= i; j++) {
 				stringBuffer.append(stopChar[j]);
 			}
-			int temp = Integer.parseInt(stopChar[i + 1] + "");
+			int temp = Integer.parseInt(String.valueOf(stopChar[i + 1]));
 			if (temp == 9) {
 				stringBuffer.append("[^0-");
 				stringBuffer.append("9])");
@@ -125,6 +125,6 @@ public class RegexTimestampRange {
 	}
 
 	private String currentTimeMillis() {
-		return System.currentTimeMillis() + "";
+		return Long.valueOf(System.currentTimeMillis()).toString();
 	}
 }
